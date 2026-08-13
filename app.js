77d530914653382add8b1bfeabf54cbc06627c8b
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-08-13 15:30:45 -0400

    Further flesh out dev environment and node vars

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import ejs from 'ejs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsPath = path.join(__dirname, 'public');
+
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`Express listening on port ${PORT}`);
+});
