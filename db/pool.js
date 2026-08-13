c36bc77eefa0f18c8300f04e23868796da66a29e
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-08-13 16:08:25 -0400

    Format controller + route + db files

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = new Pool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
